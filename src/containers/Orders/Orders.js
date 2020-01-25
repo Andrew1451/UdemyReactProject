96f85ea6c14a99fc08d4681b167df6d2f0f6fbfa
Andrew <andrew.gaynor@ymail.com>
2020-01-25 17:41:51 -0500

    change Orders from class to functional component

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useEffect } from 'react';
 import axios from '../../axios-orders';
 import { connect } from 'react-redux';
 import Order from '../../components/Order/Order';
@@ -6,23 +6,25 @@ import * as action from '../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
-class Orders extends Component {
-    componentDidMount() {
-        this.props.onFetchOrders(this.props.token, this.props.userId)
+const Orders = props => {
+    // componentDidMount() {
+    //     this.props.onFetchOrders(this.props.token, this.props.userId)
+    // }
+    useEffect(() => {
+        props.onFetchOrders(props.token, props.userId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+    let orders = <Spinner />;
+    if (!props.loading) {
+        orders = props.orders.map(order => (
+            <Order ingredients={order.ingredients} price={order.price} key={order.id} />
+        ));
     }
-    render() {
-        let orders = <Spinner />;
-        if (!this.props.loading) {
-            orders = this.props.orders.map(order => (
-                <Order ingredients={order.ingredients} price={order.price} key={order.id} />
-            ));
-        }
-        return (
-            <div>
-                {orders}
-            </div>
-        )
-    }
+    return (
+        <div>
+            {orders}
+        </div>
+    )
 }
 const mapStateToProps = state => {
     return {
